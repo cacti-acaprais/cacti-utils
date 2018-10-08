3365f7b5790018fbe2fcd3cecd04867c850c9906
Alexandre Caprais <acaprais@eurlcacti.onmicrosoft.com>
2018-10-08 09:57:50 +0200

    Job with tests

---
+++ Cacti.Utils/IJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cacti.Utils
+{
+    public interface IJob
+    {
+        Task Execute(CancellationToken token);
+    }
+}
---
+++ Cacti.Utils/Job.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cacti.Utils
+{
+    public class Job : IJob
+    {
+        private readonly Func<CancellationToken, Task> action;
+
+        public Job(Action action)
+            : this(() =>
+            {
+                action();
+                return Task.CompletedTask;
+            })
+        {
+        }
+
+        public Job(Func<Task> action)
+            : this(async (CancellationToken token) => await action())
+        {
+
+        }
+
+        public Job(IJob job)
+            : this(job.Execute)
+        {
+
+        }
+
+        public Job(Func<CancellationToken, Task> action)
+        {
+            this.action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        public async Task Execute(CancellationToken token)
+        {
+            if (!token.IsCancellationRequested)
+                await action(token);
+        }
+    }
+}
